4f487a07cd2c2f754c66546261854f199c8b1be2
DucThang0912 <ducthang140912@gmail.com>
2023-09-22 22:00:54 +0700

    add class1.cs

---
+++ WindowsFormsApp4/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp4
+{
+    internal class Class1
+    {
+        int a, b, c;
+
+        public int A { get => a; set => a = value; }
+        public int B { get => b; set => b = value; }
+        public int C { get => c; set => c = value; }
+    }
+}
